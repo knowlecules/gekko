531d1bf6a1e610861e4a728278c86182f174f6ff
Jason Rudland <knowlecules@gmail.com>
2018-12-09 18:05:32 -0500

    emitMessage was concatenating before being initialized

--- core/pipeline.js
+++ core/pipeline.js
@@ -123,7 +123,7 @@ var pipeline = (settings) => {
                 emitterMessage += sub.emitters.join(', ');
                 emitterMessage += ' ] are disabled.';
               } else {
-                emitterMessage += 'the emitting plugin (' + sub.emitter;
+                emitterMessage = 'the emitting plugin (' + sub.emitter;
                 emitterMessage += ')is disabled.'
               }
 
